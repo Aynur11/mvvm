7987b7bc80e5e4bc2664fd8cfcb6fee3c3babcd9
Aynur11 <ainur345@yandex.ru>
2015-10-06 15:31:15 +0500

    5

---
+++ UI/UI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KnockoutApi;
+
+namespace UI
+{
+    class Program
+    {
+        public class ViewModel
+        {
+            public Observable<int> a = Knockout.Observable<int>(0);
+            public Observable<int> b = Knockout.Observable<int>(0);
+            public Observable<int> result = Knockout.Observable<int>(0);
+            
+
+            public Observable<int> compute()
+            {
+                result.Value = a.Value + b.Value;
+                return result;
+            }
+            public ViewModel()
+            {
+            }
+        }
+        static void Main()
+        {
+        }
+    }
+}
