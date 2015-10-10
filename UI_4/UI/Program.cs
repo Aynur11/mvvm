8c5378afa561f7ec5a34ac0d547addb94330f1e0
Aynur11 <ainur345@yandex.ru>
2015-10-10 12:13:51 +0500

    UI_4
    Не работает chart bind

---
+++ UI_4/UI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Html;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using KnockoutApi;
+
+namespace UI
+{
+    public class ViewModel
+    {
+        public Observable<int> a = Knockout.Observable<int>(0);
+        public Observable<int> b = Knockout.Observable<int>(0);
+        public Observable<int> result = Knockout.Observable<int>(0);
+        public Observable<string> str = Knockout.Observable<string>("66 /n 4");
+
+        public Observable<int> compute()
+        {
+            result.Value = a.Value + b.Value;
+            return result;
+        }
+        public ViewModel()
+        {
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ UI_4/UI/obj/Debug/app.js
@@ -0,0 +1,31 @@
+﻿(function() {
+	'use strict';
+	var $asm = {};
+	global.UI = global.UI || {};
+	ss.initAssembly($asm, 'app');
+	////////////////////////////////////////////////////////////////////////////////
+	// UI.Program
+	var $UI_$Program = function() {
+	};
+	$UI_$Program.__typeName = 'UI.$Program';
+	$UI_$Program.$main = function() {
+	};
+	////////////////////////////////////////////////////////////////////////////////
+	// UI.ViewModel
+	var $UI_ViewModel = function() {
+		this.a = ko.observable(0);
+		this.b = ko.observable(0);
+		this.result = ko.observable(0);
+		this.str = ko.observable('66 /n 4');
+	};
+	$UI_ViewModel.__typeName = 'UI.ViewModel';
+	global.UI.ViewModel = $UI_ViewModel;
+	ss.initClass($UI_$Program, $asm, {});
+	ss.initClass($UI_ViewModel, $asm, {
+		compute: function() {
+			this.result(this.a() + this.b());
+			return this.result;
+		}
+	});
+	$UI_$Program.$main();
+})();
